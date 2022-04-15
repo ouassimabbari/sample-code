9b7dc70e476e1f48227e616623b5bca3cc596693
Achraf ABOUL <achraf.aaboul@gmail.com>
2022-04-15 10:54:18 +0000

    Added initial code

---
+++ lwc/newLocalComplaint/newLocalComplaint.js
@@ -0,0 +1,156 @@
+import { LightningElement, track, api, wire } from 'lwc';
+import { CloseActionScreenEvent } from 'lightning/actions';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { NavigationMixin } from 'lightning/navigation';
+import getProductName from '@salesforce/apex/NewLocalComplaintController.getProductName';
+import LOCAL_COMPLAINT_OBJECT from '@salesforce/schema/LocalComplaint__c';
+import ALLEGATION_FIELD from '@salesforce/schema/LocalComplaint__c.Allegation__c';
+import CALL_CENTER_SOLUTION_FIELD from '@salesforce/schema/LocalComplaint__c.CallCenterSolution__c';
+import TSG_SOLUTION_FIELD from '@salesforce/schema/LocalComplaint__c.TSG_Solution__c';
+import PRODUCT_NAME_FIELD from '@salesforce/schema/LocalComplaint__c.ProductName__c';
+import TSG_URL_FIELD from '@salesforce/schema/LocalComplaint__c.TSGURL__c';
+import LOCAL_COMPLAINT_DESCRIPTION_FIELD from '@salesforce/schema/LocalComplaint__c.LocalComplaintDescription__c';
+import CODE_NON_ROCHE_FIELD from '@salesforce/schema/LocalComplaint__c.Code_non_Roche__c';
+import SOLVED_AT_SERVICE_CENTER_FIELD from '@salesforce/schema/LocalComplaint__c.Solved_at_service_centre__c';
+import CASE_FIELD from '@salesforce/schema/LocalComplaint__c.Case__c';
+import STATUS_FIELD from '@salesforce/schema/LocalComplaint__c.Status__c';
+import COMPLAINT_SOURCE_FIELD from '@salesforce/schema/LocalComplaint__c.ComplaintSource__c';
+import FLI_SOLUTION_FIELD from '@salesforce/schema/LocalComplaint__c.FLISolution__c';
+import CASE_COUNTRY_FIELD from '@salesforce/schema/LocalComplaint__c.CaseCountry__c';
+import REPORTABLE_FIELD from '@salesforce/schema/LocalComplaint__c.Reportable__c';
+import LOCAL_COMPLAINT_CONTACT_FIELD from '@salesforce/schema/LocalComplaint__c.LocalComplaintContact__c';
+import AFFILIATE_ACTIONS_TAKEN_FIELD from '@salesforce/schema/LocalComplaint__c.Affiliate_Actions_Taken__c';
+import CLINICAL_TRIAL_ID_FIELD from '@salesforce/schema/LocalComplaint__c.ClinicalTrialID__c';
+import CLINICAL_TRIAL_PATIENT_ID_FIELD from '@salesforce/schema/LocalComplaint__c.ClinicalTrialPatientID__c';
+import EVENT_DESCRIPTION_FIELD from '@salesforce/schema/LocalComplaint__c.Event_Description__c';
+import REASON_FOR_NOT_ACCEPTING_FIELD from '@salesforce/schema/LocalComplaint__c.CHReasonforNotAccepting__c';
+import SAFETY_OFFICER_CONCLUSION_FIELD from '@salesforce/schema/LocalComplaint__c.SafetyOfficerConclusion__c';
+import CM_CLARIFY_ID_FIELD from '@salesforce/schema/LocalComplaint__c.CMClarifyID__c';
+import OWNER_ID_FIELD from '@salesforce/schema/LocalComplaint__c.OwnerId';
+import LAST_MODIFICATION_BY_ID_FIELD from '@salesforce/schema/LocalComplaint__c.LastModificationById__c';
+import EXTERNAL_REFERENCE1_FIELD from '@salesforce/schema/LocalComplaint__c.External_Reference1__c';
+import LAST_MODIFIED_DATE_FIELD from '@salesforce/schema/LocalComplaint__c.LastModifiedDate__c';
+
+export default class NewLocalComplaint extends NavigationMixin(LightningElement) {
+
+    @api recordId;
+
+    localComplaintObject = LOCAL_COMPLAINT_OBJECT;
+    allegationField = ALLEGATION_FIELD;
+    CallCenterSolutionField = CALL_CENTER_SOLUTION_FIELD;
+    TsgSolutionField = TSG_SOLUTION_FIELD;
+    ProductName = PRODUCT_NAME_FIELD;
+    TsgUrlField = TSG_URL_FIELD;
+    LocalComplaintDescriptionField = LOCAL_COMPLAINT_DESCRIPTION_FIELD;
+    CodeNonRocheField = CODE_NON_ROCHE_FIELD;
+    SolvedAtServiceCenterField = SOLVED_AT_SERVICE_CENTER_FIELD;
+    CaseField = CASE_FIELD;
+    StatusField = STATUS_FIELD;
+    ComplaintSourceField = COMPLAINT_SOURCE_FIELD;
+    FLISolutionField = FLI_SOLUTION_FIELD;
+    CaseCountryField = CASE_COUNTRY_FIELD;
+    ReportableField = REPORTABLE_FIELD;
+
+    LocalComplaintContactField = LOCAL_COMPLAINT_CONTACT_FIELD;
+    AffiliateActionsTakenField = AFFILIATE_ACTIONS_TAKEN_FIELD;
+    ClinicalTrialIDField = CLINICAL_TRIAL_ID_FIELD;
+    ClinicalTrialPatientIDField = CLINICAL_TRIAL_PATIENT_ID_FIELD;
+    EventDescriptionField = EVENT_DESCRIPTION_FIELD;
+    ReasonForNotAcceptingField = REASON_FOR_NOT_ACCEPTING_FIELD;
+    SafetyOfficerConclusionField = SAFETY_OFFICER_CONCLUSION_FIELD;
+    CMClarifyIDField = CM_CLARIFY_ID_FIELD;
+
+    OwnerIdField = OWNER_ID_FIELD;
+    LastModificationByIdField = LAST_MODIFICATION_BY_ID_FIELD;
+    ExternalReference1Field = EXTERNAL_REFERENCE1_FIELD;
+    LastModifiedDateField = LAST_MODIFIED_DATE_FIELD;
+    @track disableSaveButton = false;
+
+
+
+    @track allegationChosen = "";
+    @track callCenterSolutionChosen = "";
+    @track isOpenAllModal = false;
+    @track isOpenCallCenterSolutionModal = false;
+    @track isOpenModal = false;
+    @track productNameValue;
+
+    // GET DATA FROM APEX
+    @wire (getProductName, { caseId: '$recordId' })
+    prodNameValue(result){
+        this.productNameValue = result.data;
+        console.log('this.productNameValue ------- : ' + this.productNameValue);
+    }
+
+    //EXECUTED AFTER SAVE IS SUCCESSFUL
+    //SHOWS SUCCESS MESSAGE AND REDIRECTS TO THE DETAIL PAGE OF THE CREATED RECORD
+    handleLocalComplaintCreated(e){
+        console.log('e.detail.id ------- : ' + e.detail.id);
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: e.detail.id,
+                objectApiName: 'LocalComplaint__c',
+                actionName: 'view'
+            }
+        });
+        const event = new ShowToastEvent({
+            title: 'Success',
+            message: 'Local Complaint Created Successfully !!',
+            variant: 'success',
+            mode: 'dismissable'
+        });
+        this.dispatchEvent(event);
+    }
+
+    //NAVIGATES TO THE CUSTOM LOOKUP COMPONENT FOR THE ALLEGATION FIELD
+    handleOpenAllegationModal(){
+        console.log('isOpenAllModal');
+        this.isOpenAllModal = true;
+        this.isOpenModal = true;
+    }
+
+    //NAVIGATES TO THE CUSTOM LOOKUP COMPONENT FOR THE CALL CENTER SOLUTION FIELD
+    handleOpenCallCenterSolutionModal(){
+        console.log('isOpenCallCenterSolutionModal');
+        this.isOpenCallCenterSolutionModal = true;
+        this.isOpenModal = true;
+    }
+
+    //CLOSES THE QUICK ACTION MODAL
+    closeQuickAction() {
+        this.dispatchEvent(new CloseActionScreenEvent());
+    }
+
+    //TO EXIT THE CUSTOM LOOKUP COMPONENT FOR THE ALLEGATION FIELD AND COME BACK TO THE PARENT
+    handleCloseAllegationModal(event) {
+        this.isOpenAllModal = event.detail;
+        this.isOpenModal = false;
+    }
+
+    //TO EXIT THE CUSTOM LOOKUP COMPONENT FOR THE CALL CENTER SOLUTION FIELD AND COME BACK TO THE PARENT
+    handleCloseCallCenterSolutionModal(event) {
+        this.isOpenCallCenterSolutionModal = event.detail;
+        this.isOpenModal = false;
+    }
+
+    //TO INTERCEPT THE SAVE EVENT AND DISABLE THE SAVE BUTTON
+    handleSubmit(event){
+        event.preventDefault();       // stop the form from submitting
+        const fields = event.detail.fields;
+        console.log('fieldssss --------- : ' + JSON.stringify(fields));
+        this.disableSaveButton = true;
+        this.template.querySelector('lightning-record-edit-form').submit(fields);
+    }
+
+    //GETS THE CODE CHOSEN IN THE CUSTOM LOOKUP COMPONENT FOR THE ALLEGATION FIELD
+    handleSelectAllegation(event){
+        this.allegationChosen = event.detail;
+    }
+
+    //GETS THE CODE CHOSEN IN THE CUSTOM LOOKUP COMPONENT FOR THE CALL CENTER SOLUTION FIELD
+    handleSelectCallCenterSolution(event){
+        this.callCenterSolutionChosen = event.detail;
+    }
+
+}
